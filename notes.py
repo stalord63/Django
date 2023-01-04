912e1d2f8617405e1512ddcad8a2ed58d567d637
stalord63 <adi06031998@gmail.com>
2023-01-04 23:45:37 +0530

    adding notes

---
+++ notes.py
@@ -0,0 +1,15 @@
+#django-admin startproject MyPage
+#to make python project
+
+#python manage.py runserver
+#to start a dev server
+
+#python manage.py startapp challenges
+#to create app
+
+"""apps are building blocks of the project"""
+
+#app=module
+#url are the routes ,view are the action to the routes
+
+#views can be function or class,it handle request or response,interact to db,running business logic etc
